71ac84d9da7ae744f289604ec35cf8d4c3f36a22
brian.maina@student.moringaschool.com <brianmaina2>
2022-11-22 22:54:47 +0300

    complitted assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+//multiplication
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Number . isInteger(Math.random()* 10)+1;
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3%num4;
+
+let max = Math.max(12,6,7,4,15,17,19,20)
+max;
